9d3e6ede94ad52daffc3f06304fa37572b7e6c4b
akiogitgit <akimaron2828@icloud.com>
2022-09-24 09:58:11 +0900

    feat: shadowを修正

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,7 +17,7 @@ export const Layout: FC<Props> = ({ children }) => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <header className='bg-blue-500 shadow-md w-full py-2 top-0 z-100 duration-300 group fixed hover:bg-red-500'>
+      <header className='bg-blue-500 shadow-lg w-full py-2 top-0 z-100 duration-300 group fixed hover:bg-red-500'>
         <nav className='flex mx-5 items-center justify-between'>
           <h1 className='cursor-pointer mt-0 text-white mb-1 transform text-2xl duration-500 group-hover:(rotate-360 translate-x-20 scale-125) '>
             <span className='font-mono'>気分</span>
