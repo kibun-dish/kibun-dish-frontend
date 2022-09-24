35286746d2ef293fe181a0e04dfd118f16301ef1
akiogitgit <akimaron2828@icloud.com>
2022-09-24 10:21:58 +0900

    feat: 動画追加

---
+++ src/components/Youtube.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export const Youtube = () => {
+  return (
+    <div>
+      <iframe
+        id='player'
+        width='300'
+        height='200'
+        src={"https://www.youtube.com/embed/" + "iNA9wnuQ4Wg"}
+        frameBorder='0'
+        allowFullScreen
+      />
+      <iframe
+        id='player'
+        width='300'
+        height='200'
+        src={"https://www.youtube.com/embed/" + "4WJS7iYZ3a8"}
+        frameBorder='0'
+        allowFullScreen
+      />
+    </div>
+  )
+}
